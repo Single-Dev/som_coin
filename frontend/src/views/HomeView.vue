24d89c7035b90b30672767ad7fcee53e757c6403
bekzodbek2006 <bekzodbekk2006@gmail.com>
2024-04-03 18:24:25 +0500

    inital commit

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+   <Count/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Count from '@/components/count.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Count
+  }
+}
+</script>
