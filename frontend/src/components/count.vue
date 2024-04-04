90db7a90657562fe5acb91161e1dbc5505f6be86
bekzodbek2006 <bekzodbekk2006@gmail.com>
2024-04-04 23:41:26 +0500

    simple changes

--- frontend/src/components/count.vue
+++ frontend/src/components/count.vue
@@ -1,14 +1,14 @@
 <template>
-    <div>
-        <h2>Balansingiz: {{ count }}</h2>
-        <button @click="CountInc">Tap</button>
+    <div class="gradient-custom">
+        <h2>Som coin: {{ count }}</h2>
+        <img class="coin" @click="CountInc" src="../../public/img/icons/sticker.webp" alt="somcoin">
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            count: 0,
+            count: localStorage.getItem('count'),
         }
     },
     methods:{
@@ -16,9 +16,23 @@ export default {
             this.count ++
             localStorage.setItem('count', this.count)
         }
-    }
+    },
 }
 </script>
-<style lang="">
-    
+<style scoped>
+    .gradient-custom{
+        width: 100%;
+        height: 90vh;
+        background-image: linear-gradient(-20deg, #ff2846 0%, #6944ff 100%);
+        padding-top:100px;
+        padding-bottom:100px;
+    }
+
+    .coin{
+        width:150px;
+        border:none;
+        border-radius:50%;
+        color:#fff;
+        background:#2c3e50;
+    }
 </style>
